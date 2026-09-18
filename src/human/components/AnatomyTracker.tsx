0ef2419f2089977a34edf164446484582480eadf
aaitorvalido <aitorvalidogonzalEz@gmail.com>
2026-09-18 12:46:01 +0100

    1.1

--- src/human/components/AnatomyTracker.tsx
+++ src/human/components/AnatomyTracker.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import  { useEffect, useRef, useState } from "react";
 import '../../styles/Anatomy.css';
 
 import { ORGAN_SECTIONS, TIMELINE, ZOOMS, DAMAGE, HEALTH } from '../data/anatomyData';
